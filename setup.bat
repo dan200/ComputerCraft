82631988911fc8df0f36831baa1cf61ca7e13c0f
apemanzilla <dominic@marcuse.us>
2017-05-04 16:28:47 -0400

    Remove unnecessary setup script

--- setup.bat
+++
@@ -1,7 +0,0 @@
-echo "Setting up IntelliJ development environment with gradle..."
-rmdir /s /q .\build
-call gradlew.bat --stacktrace setupDecompWorkspace --refresh-dependencies
-call gradlew.bat --stacktrace cleanIdea idea
-
-echo "Done."
-pause
