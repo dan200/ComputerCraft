a2fd0b1f7f70714d21d5293373b16516d68c2f30
SquidDev <bonzoweb@hotmail.co.uk>
2017-05-07 01:18:59 +0100

    Use block comment instead of Javadoc for license

--- src/main/java/dan200/computercraft/shared/pocket/peripherals/PocketModemPeripheral.java
+++ src/main/java/dan200/computercraft/shared/pocket/peripherals/PocketModemPeripheral.java
@@ -5,7 +5,7 @@ import dan200.computercraft.shared.peripheral.modem.WirelessModemPeripheral;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-/**
+/*
  * This file is part of ComputerCraft - http://www.computercraft.info
  * Copyright Daniel Ratcliffe, 2011-2016. Do not distribute without permission.
  * Send enquiries to dratcliffe@gmail.com
