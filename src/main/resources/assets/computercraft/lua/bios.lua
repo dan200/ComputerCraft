9394a354d539d1249f83847d7710394f819d134a
Wilma456 <wilma456@gmx.de>
2017-06-06 16:26:28 +0200

    Remove term.isColour() check

--- src/main/resources/assets/computercraft/lua/bios.lua
+++ src/main/resources/assets/computercraft/lua/bios.lua
@@ -814,30 +814,29 @@ if bAPIError then
 end
 
 -- Set default settings
+settings.set( "bios.use_multishell", true )
 settings.set( "shell.allow_startup", true )
 settings.set( "shell.allow_disk_startup", (commands == nil) )
 settings.set( "shell.autocomplete", true )
+settings.set( "shell.promptColor", "yellow" )
+settings.set( "shell.textColor", "white" )
+settings.set( "shell.backgroundColor", "black" )
 settings.set( "edit.autocomplete", true ) 
 settings.set( "edit.default_extension", "lua" )
+settings.set( "edit.backgroundColor", "black" )
+settings.set( "edit.textColor", "white" )
+settings.set( "edit.highlightColor", "yellow" )
+settings.set( "edit.keywordColor", "yellow" )
+settings.set( "edit.commentColor", "green" )
+settings.set( "edit.stringColor", "red" )
 settings.set( "paint.default_extension", "nfp" )
 settings.set( "lua.autocomplete", true )
+settings.set( "lua.promptColor", "yellow" )
+settings.set( "lua.textColor", "white" )
 settings.set( "list.show_hidden", false )
-if term.isColour() then
-    settings.set( "bios.use_multishell", true )
-    settings.set( "list.file_color", "white" )
-    settings.set( "list.directory_color", "green" )
-    settings.set( "edit.backgroundColor", "black" )
-    settings.set( "edit.textColor", "white" )
-    settings.set( "edit.highlightColor", "yellow" )
-    settings.set( "edit.keywordColor", "yellow" )
-    settings.set( "edit.commentColor", "green" )
-    settings.set( "edit.stringColor", "red" )
-    settings.set( "shell.promptColor", "yellow" )
-    settings.set( "shell.textColor", "white" )
-    settings.set( "shell.backgroundColor", "black" )
-    settings.set( "lua.promptColor", "yellow" )
-    settings.set( "lua.textColor", "white" )
-end
+settings.set( "list.file_color", "white" )
+settings.set( "list.directory_color", "green" )
+
 if _CC_DEFAULT_SETTINGS then
     for sPair in string.gmatch( _CC_DEFAULT_SETTINGS, "[^,]+" ) do
         local sName, sValue = string.match( sPair, "([^=]*)=(.*)" )
