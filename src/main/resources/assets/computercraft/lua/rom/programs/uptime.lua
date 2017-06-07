73c92633cb712b36f16bb2ff855e80cd4c48e4bc
Wilma456 <wilma456@gmx.de>
2017-06-07 17:40:37 +0200

    Add uptime program

---
+++ src/main/resources/assets/computercraft/lua/rom/programs/uptime.lua
@@ -0,0 +1,8 @@
+local function formatUptime( nTime )
+    local nHour = math.floor( nTime/60/60 )
+    local nMinute = math.floor( (nTime/60)  ) - nHour*60
+    local nSecond = math.floor( nTime ) - nMinute*60 - nHour*60*60
+    return nHour..":"..nMinute..":"..nSecond
+end
+
+print( "The Computer runs "..formatUptime( os.clock() ) )
