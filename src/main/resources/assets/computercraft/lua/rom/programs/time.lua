3413e35112fa97721f51737ea76f6a013a457b96
Wilma456 <wilma456@gmx.de>
2017-06-20 15:22:30 +0200

    Add sources to time

--- src/main/resources/assets/computercraft/lua/rom/programs/time.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/time.lua
@@ -1,3 +1,15 @@
-local nTime = os.time()
-local nDay = os.day()
+local tArgs = { ... }
+local source
+if tArgs[1] == nil then
+    source = "ingame"
+else
+    if tArgs[1] == "ingame" or tArgs[1] == "local" or tArgs[1] == "utc" then
+        source = tArgs[1]
+    else
+        printError("Not a valid source")
+        return
+    end
+end
+local nTime = os.time( source )
+local nDay = os.day( source )
 print( "The time is "..textutils.formatTime( nTime, false ).." on Day "..nDay )
