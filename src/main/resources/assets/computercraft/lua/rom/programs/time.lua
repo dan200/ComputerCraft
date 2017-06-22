e1eea4faecb8d9eaf46098ec734dae7e7d4ecae1
Wilma456 <wilma456@gmx.de>
2017-06-22 14:10:26 +0200

    Add Sources to error and better coding style

--- src/main/resources/assets/computercraft/lua/rom/programs/time.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/time.lua
@@ -1,15 +1,15 @@
 local tArgs = { ... }
-local source
+local sSource
 if tArgs[1] == nil then
-    source = "ingame"
+    sSource = "ingame"
 else
     if tArgs[1] == "ingame" or tArgs[1] == "local" or tArgs[1] == "utc" then
-        source = tArgs[1]
+        sSource = tArgs[1]
     else
-        printError("Not a valid source")
+        printError("Not a valid source (ingame/local/utc)")
         return
     end
 end
-local nTime = os.time( source )
-local nDay = os.day( source )
+local nTime = os.time( sSource )
+local nDay = os.day( sSource )
 print( "The time is "..textutils.formatTime( nTime, false ).." on Day "..nDay )
