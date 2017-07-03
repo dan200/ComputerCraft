44fe799d671d06b54c29f761bffdde2d77236f36
ecktec <ecktec@users.noreply.github.com>
2017-07-03 21:09:31 +0200

    Now acceps not only boot but also boot.lua

--- src/main/resources/assets/computercraft/lua/bios.lua
+++ src/main/resources/assets/computercraft/lua/bios.lua
@@ -186,6 +186,8 @@ end
 local bootfile
 if fs.exists( "boot" ) and not fs.isDir( "boot" ) then
     bootfile = fs.open( "boot", "r" )
+elseif fs.exists( "boot.lua" ) and not fs.isDir( "boot.lua" ) then
+    bootfile = fs.open( "boot.lua", "r" )
 else
     bootfile = fs.open( "rom/boot.lua", "r" )
 end
