2bd9ac04b127e2f6af92690764cb3ebf611a0e92
EveryOS EveryPlatform <j.gronn4159@lakewoodcityschools.org>
2018-01-31 21:07:26 -0500

    HTTP type stuff everybody would understand

--- src/main/resources/assets/computercraft/lua/bios.lua
+++ src/main/resources/assets/computercraft/lua/bios.lua
@@ -699,8 +699,8 @@ end
 if http then
     local nativeHTTPRequest = http.request
 
-    local function wrapRequest( _url, _post, _headers, _binary )
-        local ok, err = nativeHTTPRequest( _url, _post, _headers, _binary )
+    local function wrapRequest( _url, _post, _headers, _binary, _mode, _followRedirects )
+        local ok, err = nativeHTTPRequest( _url, _post, _headers, _binary, _mode, _followRedirects )
         if ok then
             while true do
                 local event, param1, param2, param3 = os.pullEvent()
@@ -743,20 +743,35 @@ if http then
         return wrapRequest( _url, _post or "", _headers, _binary)
     end
 
-    http.request = function( _url, _post, _headers, _binary )
+    http.request = function( _url, _post, _headers, _binary, _mode, _followRedirects ) --The bios wrappers, source of much frustration when coding on java side.
         if type( _url ) ~= "string" then
-            error( "bad argument #1 (expected string, got " .. type( _url ) .. ")", 2 ) 
+            error( "bad argument #1 (expected string, got " .. type( _url ) .. ")", 2 )
         end
         if _post ~= nil and type( _post ) ~= "string" then
-            error( "bad argument #2 (expected string, got " .. type( _post ) .. ")", 2 ) 
+            error( "bad argument #2 (expected string, got " .. type( _post ) .. ")", 2 )
         end
         if _headers ~= nil and type( _headers ) ~= "table" then
-            error( "bad argument #3 (expected table, got " .. type( _headers ) .. ")", 2 ) 
+            error( "bad argument #3 (expected table, got " .. type( _headers ) .. ")", 2 )
         end
-        if _binary ~= nil and type( _binary ) ~= "boolean" then
-            error( "bad argument #4 (expected boolean, got " .. type( _binary ) .. ")", 2 ) 
+        if _mode ~= nil and type( _mode ) ~= "string" then
+            error( "bad argument #5 (expected string, got " .. type( _binary ) .. ")", 2 )
         end
-        local ok, err = nativeHTTPRequest( _url, _post, _headers, _binary )
+        if _followRedirects ~= nil and type( _followRedirects ) ~= "boolean" then
+            error( "bad argument #6 (expected boolean, got " .. type( _binary ) .. ")", 2 )
+        end
+        _mode = tostring(_mode) or ""
+        local md = _mode:upper()
+        local wl = {
+            ["GET"] = true,
+            ["POST"] = true,
+            ["HEAD"] = true,
+            ["OPTIONS"] = true,
+            ["PUT"] = true,
+            ["DELETE"] = true
+            ["TRACE"] = true
+        }
+        if not wl[md] then error("Unsuported opperation" .. ())
+        local ok, err = nativeHTTPRequest( _url, _post, _headers, _binary, _mode, _followRedirects )
         if not ok then
             os.queueEvent( "http_failure", _url, err )
         end
@@ -927,7 +942,7 @@ if bAPIError then
     term.setCursorPos( 1,1 )
 end
 
--- Set default settings
+-- Set default settings  //What if we added an security.http_enable for each cpu as well as the hard coded one and then wrapped settings to tell the user attempted to change it?
 settings.set( "shell.allow_startup", true )
 settings.set( "shell.allow_disk_startup", (commands == nil) )
 settings.set( "shell.autocomplete", true )
