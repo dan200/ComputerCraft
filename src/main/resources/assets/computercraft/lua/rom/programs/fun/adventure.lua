bbe7a4c11c4c0fc5ae3c040c3374cf8a52922b64
Daniel Ratcliffe <dratcliffe@gmail.com>
2024-05-13 12:47:31 +0100

    Allow planks to be used for building in "adventure"

--- src/main/resources/assets/computercraft/lua/rom/programs/fun/adventure.lua
+++ src/main/resources/assets/computercraft/lua/rom/programs/fun/adventure.lua
@@ -105,6 +105,7 @@ local items = {
     },
     ["some planks"] = {
         aliases = { "planks", "wooden planks", "wood planks" },
+        material = true,
         desc = "You could easilly craft these planks into sticks.",
     },
     ["some sticks"] = {
